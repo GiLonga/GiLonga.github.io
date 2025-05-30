26973e12c21e17a72fafdc30038f51ab700b5c7b
GiLonga <123774943+GiLonga@users.noreply.github.com>
2025-05-30 12:53:52 +0000

    Deploying to gh-pages from @ GiLonga/GiLonga.github.io@9aa38a38272f94049b52a6c69ae9f23aa987462f 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -24,8 +24,8 @@ ninja.data = [{
             window.location.href = "/publications/";
           },
         },{id: "nav-projects",
-          title: "projects",
-          description: "A growing collection of your cool projects.",
+          title: "Projects",
+          description: "Explore of my long-term projects.",
           section: "Navigation",
           handler: () => {
             window.location.href = "/projects/";
@@ -439,9 +439,9 @@ ninja.data = [{
           section: "News",},{id: "news-achived-2nd-place-award-in-the-feta-challenge-at-miccai-2024",
           title: 'Achived 2nd Place Award in the Feta Challenge at MICCAI 2024.',
           description: "",
-          section: "News",},{id: "projects-project-1",
-          title: 'project 1',
-          description: "with background image",
+          section: "News",},{id: "projects-geomfum",
+          title: 'GeomFum',
+          description: "Geometry processing with functional maps.",
           section: "Projects",handler: () => {
               window.location.href = "/projects/1_project/";
             },},{id: "projects-project-2",
@@ -489,14 +489,28 @@ ninja.data = [{
         title: 'email',
         section: 'Socials',
         handler: () => {
-          window.open("mailto:%79%6F%75@%65%78%61%6D%70%6C%65.%63%6F%6D", "_blank");
+          window.open("mailto:%67%69%6F%72%67%69%6F.%6C%6F%6E%67%61%72%69%31@%67%6D%61%69%6C.%63%6F%6D", "_blank");
         },
       },{
-        id: 'social-inspire',
-        title: 'Inspire HEP',
+        id: 'social-github',
+        title: 'GitHub',
         section: 'Socials',
         handler: () => {
-          window.open("https://inspirehep.net/authors/1010907", "_blank");
+          window.open("https://github.com/GiLonga", "_blank");
+        },
+      },{
+        id: 'social-linkedin',
+        title: 'LinkedIn',
+        section: 'Socials',
+        handler: () => {
+          window.open("https://www.linkedin.com/in/GiLonga", "_blank");
+        },
+      },{
+        id: 'social-orcid',
+        title: 'ORCID',
+        section: 'Socials',
+        handler: () => {
+          window.open("https://orcid.org/0000-0002-2086-9091", "_blank");
         },
       },{
         id: 'social-rss',
@@ -510,14 +524,14 @@ ninja.data = [{
         title: 'Google Scholar',
         section: 'Socials',
         handler: () => {
-          window.open("https://scholar.google.com/citations?user=qc6CJjYAAAAJ", "_blank");
+          window.open("https://scholar.google.com/citations?user=7Am1uTIAAAAJ", "_blank");
         },
       },{
         id: 'social-custom_social',
         title: 'Custom_social',
         section: 'Socials',
         handler: () => {
-          window.open("https://www.alberteinstein.com/", "_blank");
+          window.open("https://sites.google.com/site/melzismn", "_blank");
         },
       },{
       id: 'light-theme',
