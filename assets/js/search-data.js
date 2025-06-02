b852208fe6da500bc68afc981033b636f284301c
GiLonga <123774943+GiLonga@users.noreply.github.com>
2025-06-02 19:51:57 +0000

    Deploying to gh-pages from @ GiLonga/GiLonga.github.io@6d47657863ef308df1a72ceb59c6d7b79fa13038 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -37,7 +37,14 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/cv/";
           },
-        },{id: "post-a-post-with-plotly-js",
+        },{id: "dropdown-bookshelf",
+              title: "Bookshelf",
+              description: "",
+              section: "Dropdown",
+              handler: () => {
+                window.location.href = "/books/";
+              },
+            },{id: "post-a-post-with-plotly-js",
         
           title: "a post with plotly.js",
         
