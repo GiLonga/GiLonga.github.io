9265e11500be19c61cfc0957a473ca6e2c42dd8d
GiLonga <123774943+GiLonga@users.noreply.github.com>
2025-06-03 21:29:04 +0000

    Deploying to gh-pages from @ GiLonga/GiLonga.github.io@c5ceec5ac7870fe011a8aa23af2fe87b78495740 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -32,7 +32,7 @@ ninja.data = [{
           },
         },{id: "nav-cv",
           title: "CV",
-          description: "This is a description of the page. You can modify it in &#39;_pages/cv.md&#39;. You can also change or remove the top pdf download button.",
+          description: "Here you can find my full CV.",
           section: "Navigation",
           handler: () => {
             window.location.href = "/cv/";
@@ -294,7 +294,7 @@ ninja.data = [{
         section: "Posts",
         handler: () => {
           
-            window.location.href = "/assets/pdf/example_pdf.pdf";
+            window.location.href = "/assets/pdf/Giorgio_Longari_CV.pdf";
           
         },
       },{id: "post-a-post-with-diagrams",
